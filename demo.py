edebc47d3c8d79fec243d185f5e983aa2cb5d116
Juan David Gamboa Saenz <juandavidgamboasaenz@gmail.com>
2026-09-17 12:37:12 -0500

    Renamed file to demo.py added generation phrase and added similarities to the return statement in def retrieve function

--- demo.py
+++ demo.py
@@ -42,7 +42,31 @@ def retrieve(query, top_n=3):
     # Sort by similarity in descending order, because higher similarity means more relevant chunks
     similarities.sort(key=lambda x: x[1], reverse=True)
     # Finally, return the top N most relevant chunks
-    return [:top_n]
+    return similarities[:top_n]
 
 ## Generation phrase
 
+input_query = input('Ask me a question: ')
+retrieved_knowledge = retrieve(input_query)
+
+print('Retrieved knowledge:')
+for chunk, similarity in retrieved_knowledge:
+    print(f' - (similarity: {similarity:.2f}) {chunk}')
+
+instruction_prompt = f'''You are a helpful chatbot.
+Use only the following pieces of context to answer the question. Don't make up any new information:
+{'\n'.join([f' - {chunk}' for chunk, similarity in retrieved_knowledge])}
+'''
+stream = ollama.chat(
+    model=LANGUAGE_MODEL,
+    messages=[
+        {'role': 'system', 'content': instruction_prompt},
+        {'role': 'user', 'content': input_query},
+    ],
+    stream=True,
+)
+
+# Print the response from the chatbot in real-time
+print('Chatbot response:')
+for chunk in stream:
+    print(chunk['message']['content'], end='', flush=True)
